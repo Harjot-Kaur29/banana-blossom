fddadb9baaf96a7bd96b11399d207043085f74d5
Harjot-Kaur29 <harjotkaur291002@gmail.com>
2025-05-31 20:31:14 +0530

    feat:hero-section fixed

--- banana-blossom/src/components/Hero/Hero.jsx
+++ banana-blossom/src/components/Hero/Hero.jsx
@@ -13,36 +13,37 @@ const images = [
 ];
 
 const HeroSection = () => {
-  const settings = {
+    const settings = {
     dots: true,
     fade: true,
     infinite: true,
-    speed: 500,
+    speed: 1000,          
     slidesToShow: 1,
     slidesToScroll: 1,
-    waitForAnimate: false,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: true,
+    autoplaySpeed: 2000,  
+    pauseOnHover: false,
   };
+
 
   return (
     <div className={styles.heroWrapper}>
       <Slider {...settings}>
         {images.map((img, index) => (
           <div key={index} className={styles.heroSlide}>
-            <img
-              src={img}            
-              alt={`Hero slide ${index + 1}`}
-              className={styles.heroImage}
-            />
-            <div className={styles.heroContent}>
-              <h1>BANANA BLOSSOM</h1>
-              <p>ROOFTOP FINE DINE & RESTAURANT</p>
-              <button className={styles.viewMenu}>View Menu</button>
-            </div>
-          </div>
+  <img
+    src={img}
+    alt={`Hero slide ${index + 1}`}
+    className={styles.heroImage}
+  />
+  <div className={styles.overlay}></div> {/* darkens background */}
+  <div className={styles.heroContent}>
+    <h1>BANANA BLOSSOM</h1>
+    <p>ROOFTOP FINE DINE & RESTAURANT</p>
+    <button className={styles.viewMenu}>View Menu</button>
+  </div>
+</div>
         ))}
       </Slider>
     </div>
