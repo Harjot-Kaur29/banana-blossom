1ee4e5ab96bb5bd7d9be33473bc8e510728f438a
Harjot-Kaur29 <harjotkaur291002@gmail.com>
2025-06-01 23:29:38 +0530

    menu-page

--- banana-blossom/src/App.jsx
+++ banana-blossom/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import Navbar from './components/NavBar/NavBar';
-import Hero from './components/Hero/Hero';
-import InfoSection from './components/InfoSection/InfoSection';
-import Footer from './components/Footer/Footer';
+// import Navbar from './components/NavBar/NavBar';
+// import Hero from './components/Hero/Hero';
+// import InfoSection from './components/InfoSection/InfoSection';
+// import Footer from './components/Footer/Footer';
+import MenuPage from './components/MenuPage/MenuPage';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <MenuPage/>
+      {/* <Navbar />
        <Hero /> 
       <InfoSection />
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 };
